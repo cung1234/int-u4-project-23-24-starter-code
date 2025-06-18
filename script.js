0a8c6f17c92c754d32fffe6652e26128122c0328
cung1234 <cnbawi08@gmail.com>
2025-06-18 14:02:05 -0400

    Done

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
 // JavaScript goes here.
-let East = document.querySelector("#east-btn")
+// my buttons
+let east = document.querySelector("#east-btn")
 let West = document.querySelector("#west-btn")
 let Yellow = document.querySelector("#yellow-btn")
 let Blue = document.querySelector("#blue-btn")
+let minor = document.querySelector("#minor");
+let adult = document.querySelector("#adult");
+let start = document.querySelector("#start-btn");
+
+//my screen
+let question3 = document.querySelector(".question3");
 let question2 = document.querySelector(".question2")
 let question1 = document.querySelector(".question1")
 let results = document.querySelector(".result")
+let startScreen = document.querySelector(".start-screen")
+
+//Allows to differentiate endings
 let selectedDirection = "";
+
+//changes end screen picture
 let image = document.querySelector("#result-img");
+
+// Allows picture to appear in ending
 let ending = document.querySelector(".text");
+
+// removes box at the end screen
 let question = document.querySelector(".questions");
+
+// to change background color
 let body = document.querySelector("body");
 
-East.addEventListener('click', function() {
+
+start.addEventListener("click", function() {
+    console.log("start");
+    startScreen.style.display = "none";
+    question3.style.display = "block";
+    question.style.display = "block";
+    question1.style.display = "none";
+    question2.style.display = "none";
+});
+
+
+minor.addEventListener("click", function() {
+    question1.style.display = "block";
+    question3.style.display = "none";
+});
+
+adult.addEventListener("click", function() {
+    question1.style.display = "block";
+    question3.style.display = "none";
+});
+
+east.addEventListener('click', function() {
     question2.style.display = "block";
     question1.style.display = "none";
     selectedDirection = "east";
